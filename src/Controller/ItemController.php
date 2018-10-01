2d55092678b4a3cc7a3a71b5d6693c5979a62c2d
Raphael Reboullet <raphaelreboullet@gmail.com>
2018-10-01 17:17:12 +0200

    quete MVC2

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,8 +1,12 @@
 <?php
+
+namespace src\ItemController;
 // src/Controller/ItemController.php
 require __DIR__ . '/../Model/ItemManager.php';
 
 $items = selectAllItems();
 
 require __DIR__ . '/../View/item.php';
+
+class ItemController{}
 ?>
--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -1,14 +1,21 @@
 <?php
+
+namespace src\ItemManager;
+
 // src/Model/ItemManager.php
 require __DIR__ . '/../../app/db.php';
 
-// récupération de tous les items
-function selectAllItems() :array
+class ItemManager{
+
+
+
+public function selectAllItems() :array
 {
     $pdo = new \PDO(DSN, USER, PASS);
     $query = "SELECT * FROM item";
     $res = $pdo->query($query);
     return $res->fetchAll();
+}
 }
 
 ?>
